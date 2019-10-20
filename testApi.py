c2e20625698bd2777ab9ef3ea85973e228715ef0
alannatodd <agt312@nyu.edu>
2019-10-20 17:29:57 -0400

    better flow, error handling

--- testApi.py
+++ testApi.py
@@ -4,8 +4,6 @@ import os
 import sys
 import json 
 
-
-
 load_dotenv()
 api = twitter.Api(consumer_key=os.getenv('CON_KEY'),
                   consumer_secret=os.getenv('CON_SECRET'),
@@ -14,13 +12,25 @@ api = twitter.Api(consumer_key=os.getenv('CON_KEY'),
 
 
 def get_tweets(api=None, screen_name=None):
-    timeline = api.GetUserTimeline(screen_name=screen_name, count=20, include_rts=False, exclude_replies=True)
-    cleanedTimeline = []
+    listLen = 0
+    pullCount = 20
+    checkTimeline = api.GetUserTimeline(screen_name=screen_name, count=pullCount)
+    if (len(checkTimeline) == 0):
+        return ["Error: This user has not tweeted"]
+    else:
+        while listLen == 0:
+            timeline = api.GetUserTimeline(screen_name=screen_name, count=pullCount, include_rts=False, exclude_replies=True)
+            listLen = len(timeline)
+            if listLen > 0:
+                cleanedTimeline = []
+                for tweet in timeline:
+                    parsedLine = ('@{screenname}: {text}\n\n <3 {favorites}   {time}\n\n\n'.format(screenname=screen_name.encode('utf-8'), text=tweet.text.encode('utf-8'), favorites=tweet.favorite_count, time=tweet.created_at))
+                    cleanedTimeline.append(parsedLine)
+                return cleanedTimeline
+            else:
+                pullCount += 20
     #earliest_tweet = min(timeline, key=lambda x: x.id).id
     #print("getting tweets before:", earliest_tweet)
-    for tweet in timeline:
-        parsedLine = ('@{screenname}: {text}\n\n <3 {favorites}   {time}\n\n\n'.format(screenname=screen_name.encode('utf-8'), text=tweet.text.encode('utf-8'), favorites=tweet.favorite_count, time=tweet.created_at))
-        cleanedTimeline.append(parsedLine)
         #print(parsedLine)
     #while True:
         #tweets = api.GetUserTimeline(
@@ -35,7 +45,7 @@ def get_tweets(api=None, screen_name=None):
         #    print("getting tweets before:", earliest_tweet)
         #    timeline += tweets
 
-    return cleanedTimeline
+    #return cleanedTimeline
 
 
 if __name__ == "__main__":
@@ -45,11 +55,11 @@ if __name__ == "__main__":
                   access_token_secret=os.getenv('ACCESS_SECRET'))    
     screen_name = sys.argv[1]
     print(screen_name)
-    timeline = get_tweets(api=api, screen_name=screen_name)
+    cleanedTimeline = get_tweets(api=api, screen_name=screen_name)
+
 
     with open('testfiles/timeline.txt', 'w+') as f:
         #f.write(timeline)
-        for tweet in timeline:
-            print(tweet)
+        for tweet in cleanedTimeline:
+            #print(tweet)
             f.write(tweet)
-        #    f.write('\n')
