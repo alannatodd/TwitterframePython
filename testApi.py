e4148414c857c06fc733d5f3a28032f48db328a9
alannatodd <agt312@nyu.edu>
2019-10-20 16:50:34 -0400

    First runthrough of cleaning tweets

--- testApi.py
+++ testApi.py
@@ -14,24 +14,28 @@ api = twitter.Api(consumer_key=os.getenv('CON_KEY'),
 
 
 def get_tweets(api=None, screen_name=None):
-    timeline = api.GetUserTimeline(screen_name=screen_name, count=200)
-    earliest_tweet = min(timeline, key=lambda x: x.id).id
-    print("getting tweets before:", earliest_tweet)
+    timeline = api.GetUserTimeline(screen_name=screen_name, count=20, include_rts=False, exclude_replies=True)
+    cleanedTimeline = []
+    #earliest_tweet = min(timeline, key=lambda x: x.id).id
+    #print("getting tweets before:", earliest_tweet)
+    for tweet in timeline:
+        parsedLine = ('@{screenname}: {text}\n\n <3 {favorites}   {time}\n\n\n'.format(screenname=screen_name.encode('utf-8'), text=tweet.text.encode('utf-8'), favorites=tweet.favorite_count, time=tweet.created_at))
+        cleanedTimeline.append(parsedLine)
+        #print(parsedLine)
+    #while True:
+        #tweets = api.GetUserTimeline(
+        #    screen_name=screen_name, max_id=earliest_tweet, count=200
+        #)
+        #new_earliest = min(tweets, key=lambda x: x.id).id
 
-    while True:
-        tweets = api.GetUserTimeline(
-            screen_name=screen_name, max_id=earliest_tweet, count=200
-        )
-        new_earliest = min(tweets, key=lambda x: x.id).id
+        #if not tweets or new_earliest == earliest_tweet:
+        #    break
+        #else:
+        #    earliest_tweet = new_earliest
+        #    print("getting tweets before:", earliest_tweet)
+        #    timeline += tweets
 
-        if not tweets or new_earliest == earliest_tweet:
-            break
-        else:
-            earliest_tweet = new_earliest
-            print("getting tweets before:", earliest_tweet)
-            timeline += tweets
-
-    return timeline
+    return cleanedTimeline
 
 
 if __name__ == "__main__":
@@ -43,7 +47,9 @@ if __name__ == "__main__":
     print(screen_name)
     timeline = get_tweets(api=api, screen_name=screen_name)
 
-    with open('testfiles/timeline.json', 'w+') as f:
+    with open('testfiles/timeline.txt', 'w+') as f:
+        #f.write(timeline)
         for tweet in timeline:
-            f.write(json.dumps(tweet._json))
-            f.write('\n')
+            print(tweet)
+            f.write(tweet)
+        #    f.write('\n')
